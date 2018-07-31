2f21b65c4b21dcf096935de3b12e77cd86e4bd10
Camilo Contreras <crojas@pocketresult.com>
2018-07-30 21:39:22 -0500

    Presentational and Containter components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -43,9 +43,11 @@ class DishDetail extends Component {
         
             if (this.props.dish != null) 
              return(
+                <div className="container">
                 <div className="row">
                 {this.renderDish(this.props.dish)}
                 {this.renderComments(this.props.dish.comments)}
+                </div>
                 </div>
             );
         else
